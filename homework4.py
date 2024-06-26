cab427057af53a76524096a8cb7c2eae674fb1a4
53rNa <53rNa76@gmail.com>
2024-06-26 21:39:46 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -4,6 +4,6 @@ my_string = input("Введите текст: ")
 print (len(my_string))                          # Вывод длины введенного текста
 print (my_string.upper())                       # Вывод текста в верхнем регистре
 print (my_string.lower())                       # Вывод текста в нижнем регистре
-print (my_string.replace(' ', ''))  # Удаление в тексте всех пробелов
+print (my_string.replace(' ', ''))              # Удаление в тексте всех пробелов
 print (my_string[0])                            # Вывод первого символа из введенного текста
 print (my_string[-1])                           # Вывод последнего символа из введенного текста
